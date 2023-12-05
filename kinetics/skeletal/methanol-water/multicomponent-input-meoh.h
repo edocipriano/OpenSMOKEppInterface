5fdb4d84f4f51cea6d8495b3b66c00f63121d640
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2023-12-05 23:03:39 +0100

    Added kinetics schemes

---
+++ kinetics/skeletal/methanol-water/multicomponent-input-meoh.h
@@ -0,0 +1,30 @@
+#define NGS 20
+#define NLS 1
+
+char* gas_species[NGS] = {
+  "CO2",
+  "CH4",
+  "CH3",
+  "CH2",
+  "CH2(S)",
+  "CH3OH",
+  "CH3O",
+  "CH2OH",
+  "CH2O",
+  "HCO",
+  "N2",
+  "H2",
+  "H",
+  "O2",
+  "O",
+  "H2O",
+  "OH",
+  "H2O2",
+  "HO2",
+  "CO"
+};
+
+char* liq_species[NLS] = {
+  "CH3OH"
+};
+
