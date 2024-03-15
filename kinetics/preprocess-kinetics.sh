c89ea1e9d0b074767f994a05bfe6313bc3eaeaa2
Edoardo Cipriano <edoardo.cipriano@polimi.it>
2024-03-15 21:51:04 +0100

    Added script for automatic kinetics preprocessing

---
+++ kinetics/preprocess-kinetics.sh
@@ -0,0 +1,24 @@
+# Pre-processing of all kinetics schemes
+source ~/.zshrc
+set-opensmoke-0.21
+
+BASEDIR=${PWD}
+
+run_preprocessor_in_folders() {
+
+  echo "Preprocessing $1"
+  for folder in $1/*; do
+    cd "$folder"
+    OpenSMOKEpp_CHEMKIN_PreProcessor.sh
+    cd $BASEDIR
+  done
+
+}
+
+run_preprocessor_in_folders "one-step"
+run_preprocessor_in_folders "two-step"
+run_preprocessor_in_folders "materials"
+run_preprocessor_in_folders "skeletal"
+run_preprocessor_in_folders "evaporation"
+
+
